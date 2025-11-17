5462b0c6944f12cc0cb1d677eed1d00c06f48e18
adriangarciao2 <agarciao@depaul.edu>
2025-11-16 18:47:07 -0600

    test(d2l): add generated JavaUnicodeEscaperTest (fix package & JUnit4 compatibility)

---
+++ d2l/src/test/java/org/apache/commons/lang3/text/translate/JavaUnicodeEscaperTest.java
@@ -0,0 +1,30 @@
+package org.apache.commons.lang3.text.translate;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class JavaUnicodeEscaperTest {
+    @Test
+    public void test_above() {
+        // TODO: implement test for: public void above()
+        // Example: create instance and call method
+        // JavaUnicodeEscaper sut = new JavaUnicodeEscaper();
+        // assertEquals(expected, sut.above(...));
+    }
+
+    @Test
+    public void test_below() {
+        // TODO: implement test for: public void below()
+        // Example: create instance and call method
+        // JavaUnicodeEscaper sut = new JavaUnicodeEscaper();
+        // assertEquals(expected, sut.below(...));
+    }
+
+    @Test
+    public void test_between() {
+        // TODO: implement test for: public void between()
+        // Example: create instance and call method
+        // JavaUnicodeEscaper sut = new JavaUnicodeEscaper();
+        // assertEquals(expected, sut.between(...));
+    }
+}
